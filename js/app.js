39d00a5277d17868936f08ebb4c5f6ba0f8ff203
ZahraaEid <zahraateid@gmail.com>
2026-07-01 12:23:33 +0300

    Code finalized, Comments added

--- js/app.js
+++ js/app.js
@@ -1,275 +1,213 @@
 /*------------------------ Cached Element References ------------------------*/
 const startBtn = document.querySelector('#start-btn')
-const playAgainBtn = document.querySelector('#play-again-btn')
 const catcherElement = document.querySelector('#net-object')
 const fallingObjectsElement = document.querySelector('#falling-objects')
 const scoreElement = document.querySelector('#score')
 const livesElement = document.querySelector('#lives')
 const startScreenElement = document.querySelector('#start-screen')
-const gameBoardElement = document.querySelector('#game-container')
 const gameOverElement = document.querySelector('#game-over-screen')
+const winScreenElement = document.querySelector('#win-screen')
+const playAgainBtns = document.querySelectorAll('.play-again')
+const finalScoreElement = document.querySelector('#final-score')
+
+// console.log(startBtn, catcherElement, fallingObjectsElement, scoreElement, livesElement, startScreenElement, gameOverElement, winScreenElement, playAgainBtns, finalScoreElement)
 
 /*-------------------------------- Constants --------------------------------*/
-const keys = {
-    Left: false,
-    Right: false
-}
-
+const keys = { Left: false, Right: false }
 const fishTypes = ['🐟', '🐠', '🐡', '🦈', '🦑', '🦐']
 const bomb = '🧨'
 
 /*---------------------------- Variables (state) ----------------------------*/
 let catcher = 650
 let score = 0
-let lives = ['❤️', '❤️', '❤️']
+let lives = []
+let gameRunning = false
 
-/*-------------------------------- Functions --------------------------------*/
+let movementInterval
+let objectInterval
+let fallIntervals = []
 
+/*---------------------------- Functions ----------------------------*/
 function init() {
+    // reset all values 
+    clearInterval(movementInterval)
+    clearInterval(objectInterval)
+
+    fallIntervals.forEach(clearInterval)
+    fallIntervals = []
+
+    fallingObjectsElement.innerHTML = ''
+
     startScreenElement.classList.add('hidden')
+    gameOverElement.classList.add('hidden')
+    winScreenElement.classList.add('hidden')
+
+    catcher = 650
+    score = 0
+    lives = ['❤️', '❤️', '❤️']
+    gameRunning = true
 
     scoreElement.textContent = `Score: ${score}`
     livesElement.textContent = `Lives: ${lives}`
 
-    setInterval(catcherMovement, 16)
+    // save the catcherMovement to be executed every 16ms to movementInterval
+    movementInterval = setInterval(catcherMovement, 16)
+
     startGame()
 }
 
 function catcherMovement() {
+    if (!gameRunning) return
+
+    // if left key clicked move the catcher 5px to the left
     if (keys.Left) catcher -= 5
+    // if right key clicked move the catcher 5px to the right
     if (keys.Right) catcher += 5
 
-    if (catcher < 300) catcher = 300
-    if (catcher > 976) catcher = 976
+    // stops catcher from going out of the container
+    if (catcher < 275) catcher = 276
+    if (catcher > 925) catcher = 926
 
     catcherElement.style.left = `${catcher}px`
 }
 
 function startGame() {
     fallingObjects()
-    setInterval(fallingObjects, 1500)
+
+    // assigns the fallingObjects to be executed every 1.5s to objectInterval
+    objectInterval = setInterval(fallingObjects, 1500)
 }
 
 function fallingObjects() {
-    createObject(
-        fishTypes[Math.floor(Math.random() * fishTypes.length)]
+    createObject(Math.random() < 0.7
+        ? fishTypes[Math.floor(Math.random() * fishTypes.length)]
+        : bomb
     )
 
-    createObject(bomb)
 }
 
 function createObject(type) {
+    // creates span to add objects in html
     const item = document.createElement('span')
-
     item.textContent = type
     item.style.position = 'absolute'
     item.style.top = '0px'
     item.style.left = Math.floor(Math.random() * 650) + 'px'
 
+    // adds the span to html using AppendChild (Add span to the <p id="falling-objects"></p>)
     fallingObjectsElement.appendChild(item)
 
     let y = 0
 
     const fall = setInterval(() => {
+
+        // if game is not running stop objects from falling
+        if (!gameRunning) {
+            item.remove()
+            clearInterval(fall)
+            return
+        }
+
         y += 3
-        item.style.top = y + 'px'
+        item.style.top = `${y}px`
 
-        catching(item, fall)
+        // handleCatching function called to catch the fallen object -item- and its place after falling -fall-
+        handleCatching(item, fall)
 
+        // if fallen object falls over 600px vertically (does not appear in screen) remove it and stop its falling
         if (y > 600) {
             item.remove()
             clearInterval(fall)
         }
+
+        // 25 is falling speed, higher number will make it slow, lower number will be faster
     }, 25)
+
+    // add each fallen item into fallIntervals array
+    fallIntervals.push(fall)
 }
 
-function catching(item, fall) {
-
-
+function handleCatching(item, fall) {
+    // get catcher and fallenObjects location using .getBoundingClientRect()
     const itemRect = item.getBoundingClientRect()
     const catcherRect = catcherElement.getBoundingClientRect()
+
+    // checking if the catcher and item is near each other 
     if (
         itemRect.left < catcherRect.right &&
         itemRect.right > catcherRect.left &&
         itemRect.top < catcherRect.bottom &&
         itemRect.bottom > catcherRect.top
     ) {
-
-        if (lives.length < 1) { gameOver() }
-
+        // check if catching a bomb, minus lives (pop lives out of lives array) and if lives array is empty calls gameOver function
         if (item.textContent === bomb) {
-
             lives.pop()
             livesElement.textContent = `Lives: ${lives}`
-            if (lives.length === 0) {
-                gameOver()
-            }
+
+            if (lives.length === 0) return gameOver()
+
         } else {
+            // if not bomb add 10 points in score, update score and if score is 150 calls winGame function
             score += 10
             scoreElement.textContent = `Score: ${score}`
-        }
 
+            if (score >= 150) return winGame()
+        }
+        // otherwise remove the fallen object and delete its falling feature
         item.remove()
         clearInterval(fall)
     }
-
 }
+
+// if player wins reset all and show game winning screen
+function winGame() {
+    gameRunning = false
+
+    clearInterval(movementInterval)
+    clearInterval(objectInterval)
+
+    fallIntervals.forEach(clearInterval)
+    fallIntervals = []
+
+    finalScoreElement.textContent = score
+
+    // remove hidden from win screen class to show, and hide the game over screen
+    winScreenElement.classList.remove('hidden')
+    gameOverElement.classList.add('hidden')
+}
+
+// if player loses reset all and show the game over screen
 function gameOver() {
+    gameRunning = false
 
+    clearInterval(movementInterval)
+    clearInterval(objectInterval)
+
+    fallIntervals.forEach(clearInterval)
+    fallIntervals = []
+
+    finalScoreElement.textContent = score
+
+    // remove hidden from game over class to show, and hide the win game screen
     gameOverElement.classList.remove('hidden')
-    score = 0
-    lives = ['❤️', '❤️', '❤️']
-
+    winScreenElement.classList.add('hidden')
 }
-/*----------------------------- Event Listeners -----------------------------*/
 
+/*-------------------------------- Evennt Listeners ----------------------------*/
+// using window property to handle if a, d, left arrow, right arrow is pressed (Keydown) the keys left and right will be true and movement will start
 window.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowLeft' || e.key === 'a') keys.Left = true
     if (e.key === 'ArrowRight' || e.key === 'd') keys.Right = true
 })
 
+// if the keyup means player stopped pressing the keys (left and right) be false and movement stops
 window.addEventListener('keyup', (e) => {
     if (e.key === 'ArrowLeft' || e.key === 'a') keys.Left = false
     if (e.key === 'ArrowRight' || e.key === 'd') keys.Right = false
 })
 
+// Start btn event listener
 startBtn.addEventListener('click', init)
-playAgainBtn.addEventListener('click', init)
 
-
-// /*------------------------ Cached Element References ------------------------*/
-// const startBtn = document.querySelector('#start-btn')
-// const playAgainBtn = document.querySelector('#play-again-btn')
-// const catcherElement = document.querySelector('#net-object')
-// const fallingObjectsElement = document.querySelector('#falling-objects')
-// const scoreElement = document.querySelector('#score')
-// const livesElement = document.querySelector('#lives')
-// const startScreenElement = document.querySelector('#start-screen')
-// const gameBoardElement = document.querySelector('#game-container')
-// console.log(startBtn, playAgainBtn, catcherElement, fallingObjectsElement, scoreElement, livesElement, startScreenElement, gameBoardElement)
-
-// /*-------------------------------- Constants --------------------------------*/
-// const keys = {
-//     Left: false,
-//     Right: false
-// }
-// const fishTypes = ['🐟', '🐠', '🐡', '🦈', '🦑', '🦐']
-// const bomb = '🧨'
-// /*---------------------------- Variables (state) ----------------------------*/
-// let catcher = 650
-// let score = 0
-
-// /*-------------------------------- Functions --------------------------------*/
-// function init() {
-//     startScreenElement.classList.add('hidden')
-
-//     setInterval(catcherMovement, 16)
-//     startGame()
-// }
-
-// function catcherMovement() {
-//     // console.log(catcher)
-//     if (catcher <= 300) catcher = 301
-//     if (catcher >= 976) catcher = 977
-
-//     if (keys.Left) catcher -= 5
-//     if (keys.Right) catcher += 5
-
-//     catcherElement.style.left = `${catcher}px`
-//     // catching()
-
-// }
-
-
-// function startGame() {
-//     fallingFish()
-//     setInterval(fallingFish, 1500)
-// }
-
-
-// function fallingFish() {
-//     const fish = document.createElement('span')
-//     const bombs = document.createElement('span')
-
-//     bombs.textContent = bomb * 10
-//     fish.textContent = fishTypes[Math.floor(Math.random() * fishTypes.length)]
-
-//     fish.style.position = 'absolute'
-//     fish.style.top = '0px'
-//     fish.style.left = Math.floor(Math.random() * 650) + 'px'
-
-//     bombs.style.position = 'absolute'
-//     bombs.style.top = '0px'
-//     bombs.style.left = Math.floor(Math.random() * 650) + 'px'
-
-
-//     fallingObjectsElement.appendChild(fish)
-//     fallingObjectsElement.appendChild(bombs)
-
-//     let y = 0
-
-//     const fall = setInterval(() => {
-//         y += 3
-//         fish.style.top = y + 'px'
-//         bombs.style.top = y + 'px'
-
-
-//         catching(fish, fall)
-//         catching(bombs, fall)
-
-//         if (y > 600) {
-//             fish.remove()
-//             bombs.remove()
-//             clearInterval(fall)
-//         }
-//     }, 25)
-
-
-
-// }
-
-// function catching(fish, fall) {
-//     const fishRect = fish.getBoundingClientRect()
-//     const catcherRect = catcherElement.getBoundingClientRect()
-// const bomb =
-//     if (
-//         fishRect.left < catcherRect.right &&
-//         fishRect.right > catcherRect.left &&
-//         fishRect.top < catcherRect.bottom &&
-//         fishRect.bottom > catcherRect.top
-//     ) {
-//         score += 10
-//         scoreElement.textContent = `Score: ${score}`
-
-//         fish.remove()
-//         clearInterval(fall)
-//     }
-
-//     else if (
-//         fishRect.left < catcherRect.right &&
-//         fishRect.right > catcherRect.left &&
-//         fishRect.top < catcherRect.bottom &&
-//         fishRect.bottom > catcherRect.top
-//     ) {
-//         score += 10
-//         scoreElement.textContent = `Score: ${score}`
-
-//         fish.remove()
-//         clearInterval(fall)
-//     }
-// }
-
-// /*----------------------------- Event Listeners -----------------------------*/
-
-// window.addEventListener('keydown', (e) => {
-//     if (e.key === 'ArrowLeft' || e.key === 'a') keys.Left = true
-//     if (e.key === 'ArrowRight' || e.key === 'd') keys.Right = true
-// })
-
-// window.addEventListener('keyup', (e) => {
-//     if (e.key === 'ArrowLeft' || e.key === 'a') keys.Left = false
-//     if (e.key === 'ArrowRight' || e.key === 'd') keys.Right = false
-// })
-
-// startBtn.addEventListener('click', init)
-
+// loop each play again btns from game over and winning screen
+playAgainBtns.forEach(btn => btn.addEventListener('click', init))
